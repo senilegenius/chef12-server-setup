2e0a638373732e5fae98996088f86c256586bf2b
senilegenius <csimmons@square-root.com>
2015-09-01 10:57:31 -0500

    my initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'chef12-server-test'
+maintainer       'Square Root'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures chef12-server-test'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: chef12-server-test
+# Recipe:: default
+#
+# Copyright 2015, Square Root
+#
+# All rights reserved - Do Not Redistribute
+#
